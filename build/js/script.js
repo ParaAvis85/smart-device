eda05e10c84610e0faaf997d1785ecf852428533
ParaAvis <Grynza_let@mail.ru>
2021-02-10 18:31:20 +0300

    Добавил условие на проверку всех элементов

--- build/js/script.js
+++ build/js/script.js
@@ -14,63 +14,66 @@
   var isStorageSupport = true;
   var storage = {};
 
-  var openPopup = function () {
-    popup.classList.add('modal--show');
-    document.body.classList.add('disable-scroll');
-  };
+  if (popup && link && form && close && userName && phone && message) {
 
-  var closePopup = function () {
-    popup.classList.remove('modal--show');
-    document.body.classList.remove('disable-scroll');
-  };
+    var openPopup = function () {
+      popup.classList.add('modal--show');
+      document.body.classList.add('disable-scroll');
+    };
 
-  try {
-    storage.name = localStorage.getItem('name');
-    storage.phone = localStorage.getItem('phone');
-    storage.message = localStorage.getItem('message');
-  } catch (err) {
-    isStorageSupport = false;
-  }
+    var closePopup = function () {
+      popup.classList.remove('modal--show');
+      document.body.classList.remove('disable-scroll');
+    };
 
-  link.addEventListener('click', function (evt) {
-    evt.preventDefault();
-    openPopup();
-
-    if (storage.name) {
-      userName.value = storage.name;
-      phone.value = storage.phone;
-      message.value = storage.message;
-      message.focus();
-    } else {
-      userName.focus();
+    try {
+      storage.name = localStorage.getItem('name');
+      storage.phone = localStorage.getItem('phone');
+      storage.message = localStorage.getItem('message');
+    } catch (err) {
+      isStorageSupport = false;
     }
-  });
 
-  close.addEventListener('click', function (evt) {
-    evt.preventDefault();
-    closePopup();
-  });
-
-  form.addEventListener('submit', function () {
-    if (isStorageSupport) {
-      localStorage.setItem('name', userName.value);
-      localStorage.setItem('phone', phone.value);
-      localStorage.setItem('message', message.value);
-    }
-  });
-
-  window.addEventListener('keydown', function (evt) {
-    if (evt.keyCode === KEYCODE.esc) {
+    link.addEventListener('click', function (evt) {
       evt.preventDefault();
-      if (popup.classList.contains('modal--show')) {
+      openPopup();
+
+      if (storage.name) {
+        userName.value = storage.name;
+        phone.value = storage.phone;
+        message.value = storage.message;
+        message.focus();
+      } else {
+        userName.focus();
+      }
+    });
+
+    close.addEventListener('click', function (evt) {
+      evt.preventDefault();
+      closePopup();
+    });
+
+    form.addEventListener('submit', function () {
+      if (isStorageSupport) {
+        localStorage.setItem('name', userName.value);
+        localStorage.setItem('phone', phone.value);
+        localStorage.setItem('message', message.value);
+      }
+    });
+
+    window.addEventListener('keydown', function (evt) {
+      if (evt.keyCode === KEYCODE.esc) {
+        evt.preventDefault();
+        if (popup.classList.contains('modal--show')) {
+          closePopup();
+        }
+      }
+    });
+
+    popup.addEventListener('click', function (evt) {
+      if (evt.target === popup) {
         closePopup();
       }
-    }
-  });
-
-  popup.addEventListener('click', function (evt) {
-    if (evt.target === popup) {
-      closePopup();
-    }
-  });
+    });
+  }
 })();
