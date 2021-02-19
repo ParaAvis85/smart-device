aab2d9808eb1559e13bcf6d7c1112fc865286bfa
ParaAvis <Grynza_let@mail.ru>
2021-02-19 19:47:43 +0300

    добавил маску формы телефона

--- build/js/script.js
+++ build/js/script.js
@@ -111,3 +111,23 @@
     accordionToggleButton.addEventListener('click', toggleAccordion);
   });
 })();
+
+/*eslint-disable*/
+
+'use strict';
+
+(function () {
+  var maskedInputs = document.querySelectorAll('input[data-inputmask]');
+
+  var applyMask = function () {
+    Array.prototype.forEach.call(maskedInputs, function (input) {
+      var maskOption = {
+        mask: input.getAttribute('data-inputmask')
+      };
+      IMask(input, maskOption);
+    });
+  };
+
+  applyMask();
+
+})();
